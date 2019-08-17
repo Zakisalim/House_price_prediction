675e7c02a2c9d3b0187bbaeef5d4d05f0e536fff
Zaki Malik Salim <zaki_klakla@yahoo.com>
2019-08-17 17:39:52 -0400

    update sql code

--- zillow_houses.sql
+++ zillow_houses.sql
@@ -21,7 +21,8 @@ SELECT * FROM sqr_values;
 CREATE VIEW mlpsf_zipcode AS
 	SELECT A.date_by_month, A.pricing, A.zip_code, ROUND((A.pricing/B.pricing_per_sqft*1.0)) AS sqr_ft
 	FROM mlbsf_byzipcode A LEFT JOIN sqr_values B
-	ON A.zip_code = B.zip_code;
+	ON A.zip_code = B.zip_code
+	WHERE ROUND((A.pricing/B.pricing_per_sqft*1.0)) IS NOT NULL; 
 	
 
 SELECT * FROM mlpsf_zipcode;
