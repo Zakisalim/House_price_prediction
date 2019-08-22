0c21f51ea7a693e4829307c90344f71917e2a827
Zaki Malik Salim <zaki_klakla@yahoo.com>
2019-08-22 19:27:08 -0400

    Add files via upload

--- zillow_houses.sql
+++ zillow_houses.sql
@@ -5,6 +5,7 @@ CREATE TABLE sqr_values
     zip_code integer
 );
 
+
 CREATE TABLE zillow_zipcode
 (
     date_by_month varchar,
@@ -13,16 +14,14 @@ CREATE TABLE zillow_zipcode
     sqr_ft double precision
 );
 
-
-SELECT * FROM mlbsf_byzipcode;
+SELECT * FROM zillow_zipcode;
 
 SELECT * FROM sqr_values;
 
 CREATE VIEW mlpsf_zipcode AS
 	SELECT A.date_by_month, A.pricing, A.zip_code, ROUND((A.pricing/B.pricing_per_sqft*1.0)) AS sqr_ft
-	FROM mlbsf_byzipcode A LEFT JOIN sqr_values B
-	ON A.zip_code = B.zip_code
-	WHERE ROUND((A.pricing/B.pricing_per_sqft*1.0)) IS NOT NULL; 
+	FROM zillow_zipcode A LEFT JOIN sqr_values B
+	ON A.zip_code = B.zip_code;
 	
 
 SELECT * FROM mlpsf_zipcode;
